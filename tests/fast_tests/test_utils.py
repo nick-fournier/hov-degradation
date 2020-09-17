3dcca9ef0bc35f13853cf33864ba48662f3d9f5e
Yasharzf <y.zeinali@gmail.com>
2020-09-17 11:07:39 -0700

    added test class

---
+++ tests/fast_tests/test_utils.py
@@ -0,0 +1,24 @@
+"""Tests for the files in the hov_degradation/utils folder."""
+import unittest
+
+from hov_degradation.utils import data_preprocess
+
+
+class TestDataProcessr(unittest.TestCase):
+    """Tests the DataProcess object."""
+
+    def setUp(self):
+        # TODO yf
+        pass
+
+    def tearDown(self):
+        # TODO yf
+        pass
+
+    def test_data_process(self):
+        # TODO yf
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
