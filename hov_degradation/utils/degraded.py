69a735383587fec5211b087cb3d80013cc1359b1
Nick Fournier <nichfournier@gmail.com>
2021-02-02 09:08:27 -0800

    fix degraded analsis to exclude weekends

--- hov_degradation/utils/degraded.py
+++ hov_degradation/utils/degraded.py
@@ -1,19 +1,21 @@
 import os
-import numpy as np
 import pandas as pd
-from os import path
+from pandas.tseries.holiday import USFederalHolidayCalendar
 
 class GetDegradation:
 
-    def __init__(self, path, bad_sensors, peak_hours, saved=False, metaname='d07_text_meta_2019_11_09.txt'):
+    def __init__(self, path, bad_sensors, peak_hours, saved=False, joedays=False, metaname='d07_text_meta_2019_11_09.txt'):
         self.path = path
         self.meta = pd.read_csv(path + metaname)
         self.bad_ids = bad_sensors
         self.peak_am = peak_hours['peak_am']
         self.peak_pm = peak_hours['peak_pm']
+        self.joedays = joedays
         self.neighbors = None
+        self.raw_data = None
         self.data = None
         self.degraded = None
+        self.saved = saved
 
     def Repeat(x):
         _size = len(x)
@@ -42,94 +44,58 @@ class GetDegradation:
                                      'neighbor_ML': neighbors_id})
         df_neighbors = df_neighbors.merge(self.bad_ids, left_on='bad_HOV', right_on='ID').drop(columns='ID')
 
-        self.neighbors = df_neighbors
-        # return df_neighbors
+        # self.neighbors = df_neighbors
+        return df_neighbors
 
-    def get_sensor_data(self, saved):
+    def get_sensor_data(self):
 
         fpath = self.path + 'd07_extracted_sensors_hourly.csv'
 
-        if saved and os.path.isfile(fpath):
-                self.raw_data = pd.read_csv(fpath)
+        if self.saved and os.path.isfile(fpath):
+                df_sensors = pd.read_csv(fpath)
         else:
-            # Header
+            # Headers, file list, and ID list
             headers = pd.read_csv(self.path + "d07_hourly_headers.csv", index_col=0, header=0)
-
-            #List of all useful IDs
             id_list = self.neighbors['bad_HOV'].to_list() + self.neighbors['neighbor_ML'].to_list()
-
-            # Read files
             misconfigs = []
             gzlist = pd.Series(os.listdir(self.path))
             gzlist = gzlist[gzlist.str.contains("txt.gz")]
 
+            # Read file, Filter for misconfig'd sensors
             for gzf in gzlist:
-                # Read file
                 df_hourly = pd.read_csv(self.path + gzf, header=None, names=headers)
-
-                # Filter for misconfig'd sensors and peak hours only 6-9am and 3-6pm
                 df_hourly = df_hourly[df_hourly['Station'].isin(id_list)]
-
-                # Add to output list
                 misconfigs.append(df_hourly)
                 print("Done loadings " + gzf)
 
-            # Bind together
+            # Bind together &  Rename Station length to get rid of space
             df_sensors = pd.concat(misconfigs)
-
-            # Rename Station length to get rid of space
             df_sensors.rename(columns={"Station Length": "Length"}, inplace=True)
-
             #Save
             df_sensors.to_csv(fpath)
-            self.raw_data = df_sensors
-        # return self.raw_data
 
-    def get_filtered_data(self):
-        f_time = self.raw_data['Timestamp'].str.slice(11, 19).isin(self.peak_am + self.peak_pm)
-        df_hourly = self.raw_data[f_time]
+        # self.raw_data = df_sensors
+        return df_sensors
 
-        # Add peak hour and day indicator for aggregation later
-        df_hourly['DATE'] = df_hourly['Timestamp'].str.slice(0, 10)
-        df_hourly.loc[df_hourly['Timestamp'].str.slice(11, 19).isin(self.peak_pm), 'PEAK'] = 'PM'
-        df_hourly.loc[df_hourly['Timestamp'].str.slice(11, 19).isin(self.peak_am), 'PEAK'] = 'AM'
+    def get_vhtvmt(self, df, suffix):
+        # Calculate VMT and VHT
+        df = df.assign(VMT=df.eval('Flow * Length'))
+        df = df.assign(VHT=df.eval('(Flow * Length) / Speed'))
 
-        # Aggregation groups
-        bool = df_hourly.columns.isin(
-            ['DATE', 'PEAK', 'Station', 'District', 'Freeway', 'Direction', 'Lane Type', 'Length']
-        )
-        grp_cols = df_hourly.columns[bool].to_list()
+        # Aggregate & remove <100 observable
+        grp_cols = df.columns[~df.columns.isin(['VMT', 'VHT', 'Flow', 'Occupancy', 'Speed'])].to_list()
+        df = df.groupby(grp_cols).agg({'VMT': 'sum', 'VHT': 'sum'}).reset_index()
+        df = df[df['Observed'] == 100]
 
-        # Sum flow
-        sum_cols = list(df_hourly.columns[["Flow" in x for x in df_hourly.columns]])
+        # Calculate speed from VMT/VHT
+        df['Avg Speed'] = df.eval('VMT / VHT')
 
-        # Average speed, delay, occupancy, and observed
-        avg_cols = [False]*len(df_hourly.columns)
-        for var in ['Speed', 'Delay', 'Occupancy', 'Observed']:
-            avg_cols = avg_cols | np.array([var in x for x in df_hourly.columns])
-        avg_cols = list(df_hourly.columns[avg_cols])
-
-        # Combined aggregation
-        _agg = {**{i: 'mean' for i in avg_cols}, **{i: 'sum' for i in sum_cols}}
-
-        # Aggregate
-        df_dailypeaks = df_hourly.groupby(grp_cols).agg(_agg).reset_index()
-
-        # Keep only 100% observable
-        df_dailypeaks = df_dailypeaks[df_dailypeaks['Observed'] == 100]
-
-        #Save
-        # df_peakhours.to_csv(self.path + 'd07_processed_hourly.csv')
-
-        self.data = df_dailypeaks
-        # return self.data
-
-    def get_vhtvmt(df, suffix):
-        vmt = sum(df.eval('Flow * Length'))
-        vht = sum(df.eval('(Flow * Length) / Speed'))
+        #Calculate the sum totals
+        vmt = df['VMT'].sum()
+        vht = df['VHT'].sum()
         spd = vmt / vht
         ndays = len(df)
-        ndays_deg = sum((df.eval('Flow * Length') / df.eval('(Flow * Length) / Speed')) < 45)
+        ndays_deg = sum(df['Avg Speed'] < 45)
         perc_deg = ndays_deg / ndays
 
         dict_vhtvmt = {'VMT ' + suffix: vmt,
@@ -143,9 +109,23 @@ class GetDegradation:
         return dict_vhtvmt
 
     def get_degradation(self):
+        # Add necessary data
+        self.neighbors = self.get_neighbors()
+        self.raw_data = self.get_sensor_data()
+
         _cols = ['Flow', 'Occupancy', 'Speed']
-        base_cols = ['Station', 'District', 'Freeway', 'Direction', 'Lane Type', 'Length', 'DATE', 'PEAK']
+        base_cols = ['Station', 'District', 'Freeway', 'Direction', 'Lane Type', 'Length', 'Observed', 'DATE', 'PEAK']
         bad_cols = base_cols + _cols
+
+        # Filter peak hours & weekdays only
+        f_peaks = self.raw_data['Timestamp'].str.slice(11, 19).isin(self.peak_am + self.peak_pm)
+        f_dow = pd.to_datetime(self.raw_data['Timestamp']).dt.dayofweek < 5  # 0-4: Monday-Friday, 5-6: Saturday-Sunday
+        self.data = self.raw_data[f_dow & f_peaks]
+
+        # Add peak hour and day indicator for aggregation later
+        self.data = self.data.assign(DATE=self.data['Timestamp'].str.slice(0, 10))
+        self.data.loc[self.data['Timestamp'].str.slice(11, 19).isin(self.peak_pm), 'PEAK'] = 'PM'
+        self.data.loc[self.data['Timestamp'].str.slice(11, 19).isin(self.peak_am), 'PEAK'] = 'AM'
 
         hov_results = []
         for bad_id in self.bad_ids['ID']:
@@ -153,13 +133,21 @@ class GetDegradation:
             good_lane = self.neighbors[self.neighbors['bad_HOV'] == bad_id]['real_lane'].to_string(index=False).strip()
             good_cols = [good_lane + s for s in [' Flow', ' Occupancy', ' Speed']]
 
-            # Extract associated data
-            good_data = self.data[self.data['Station'] == good_id][base_cols + good_cols]
-            bad_data = self.data[self.data['Station'] == bad_id][bad_cols]
+            if bad_id == 769745 & self.joedays:
+                # Filter Sensor 769745 for direct comparison
+                datepath = self.path + 'joedates_for_769745.csv'
+                # datepath = 'hov_degradation/utils/joedates_for_769745.csv'
+                thesensor = self.data['Station'] == 769745
+                thedates = self.data['DATE'].isin(pd.read_csv(datepath, header=None)[0].tolist())
+                bad_data = self.data[thedates & thesensor][bad_cols]
+            else:
+                # Extract associated data & rename to match
+                bad_data = self.data[self.data['Station'] == bad_id][bad_cols]
 
-            # Rename correct lane columns
+            good_data = self.data[self.data['Station'] == good_id][base_cols + good_cols]
             good_data.rename(columns={good_cols[i]: _cols[i] for i in range(len(good_cols))}, inplace=True)
 
+            # get VMT & VMT
             output_meta = {
                 'Fwy': self.meta[self.meta['ID'] == good_id]['Fwy'].to_string(index=False).strip(),
                 'Direction': self.meta[self.meta['ID'] == good_id]['Dir'].to_string(index=False).strip(),
@@ -172,15 +160,13 @@ class GetDegradation:
                 'Correct ML lane #': good_lane,
                 'Length': self.meta[self.meta['ID'] == good_id]['Length'].to_string(index=False).strip(),
             }
-
-            output_results = {**GetDegradation.get_vhtvmt(bad_data[bad_data['PEAK'] == 'AM'], suffix='(Erroneous AM)'),
-                              **GetDegradation.get_vhtvmt(bad_data[bad_data['PEAK'] == 'AM'], suffix='(Erroneous AM)'),
-                              **GetDegradation.get_vhtvmt(bad_data[bad_data['PEAK'] == 'PM'], suffix='(Erroneous PM)'),
-                              **GetDegradation.get_vhtvmt(good_data[good_data['PEAK'] == 'AM'], suffix='(Corrected AM)'),
-                              **GetDegradation.get_vhtvmt(good_data[good_data['PEAK'] == 'PM'], suffix='(Corrected PM)'),
+            output_results = {**self.get_vhtvmt(bad_data[bad_data['PEAK'] == 'AM'], suffix='(Erroneous AM)'),
+                              **self.get_vhtvmt(bad_data[bad_data['PEAK'] == 'PM'], suffix='(Erroneous PM)'),
+                              **self.get_vhtvmt(good_data[good_data['PEAK'] == 'AM'], suffix='(Corrected AM)'),
+                              **self.get_vhtvmt(good_data[good_data['PEAK'] == 'PM'], suffix='(Corrected PM)'),
                               }
-
             output = {**output_meta, **output_results}
+
             #add results to data frame
             hov_results.append(pd.DataFrame([output]))
 
@@ -198,8 +184,24 @@ def get_dates(path):
     dates = list(map(lambda st: str.replace(st, "_", "-"), dates))
     dates = list(map(lambda st: str.replace(st, ".txt.gz", ""), dates))
     dates = dates[0] + '_to_' + dates[len(dates)-1]
-
     return dates
+
+def get_holidays(path):
+    # Initialize the dates
+    joedates = pd.read_csv(path + 'joedates_for_769745.csv',header=None)[0]
+    joedates = pd.to_datetime(joedates, format='%m/%d/%Y')
+    alldates = pd.date_range(joedates.min(), joedates.max())
+    # Holidays
+    cal = USFederalHolidayCalendar()    # cal = calendar()
+    holidays = cal.holidays(start=alldates.min(), end=alldates.max())
+
+    # Make into DataFrame
+    df = pd.DataFrame({'Date': alldates})
+    df['JoeDate'] = df['Date'].isin(joedates)
+    df['Weekday'] = df['Date'].dt.dayofweek < 5  # 0-4: Monday-Friday, 5-6: Saturday-Sunday
+    df['Holiday'] = df['Date'].isin(holidays)
+
+    return df
 
 if __name__ == '__main__':
     inpath = "../../experiments/raw data/D7/hourly/"
@@ -207,23 +209,23 @@ if __name__ == '__main__':
     # inpath = "experiments/raw data/D7/hourly/"
     # outpath = "experiments/district_7/results/"
 
-    #Peak hours
+    # df_dates = get_holidays(inpath)
+
+    # Peak hours
     peak_hours = {'peak_am': ['06:00:00', '07:00:00', '08:00:00'],
                   'peak_pm': ['15:00:00', '16:00:00', '17:00:00']}
 
-    #These are the bad IDs and suspected mislabeled lane
+    # These are the bad IDs and suspected mislabeled lane
     df_bad = pd.DataFrame(
         {'ID': [717822, 718270, 718313, 762500, 762549, 768743, 769238, 769745, 774055],
          'issue': ['Misconfigured']*9,
          'real_lane': ['Lane 1', 'Lane 2', 'Lane 1', 'Lane 2', 'Lane 1', 'Lane 4', 'Lane 1', 'Lane 3', 'Lane 1']}
     )
 
-    degraded = GetDegradation(inpath, df_bad, peak_hours)
-    degraded.get_neighbors()
-    degraded.get_sensor_data(saved=True)
-    degraded.get_filtered_data()
+    degraded = GetDegradation(inpath, df_bad, peak_hours, saved=True, joedays=False)
     results = degraded.get_degradation()
 
+    # Saving
     dates = get_dates(inpath)
     results.to_csv(outpath + 'degradation_results_' + dates + '.csv', index=False)
 
